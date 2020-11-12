b8b862af650b4f62457ee6bca0caa40bfffcc89a
Nathan-Andersen <nathan.andersen91@gmail.com>
2020-11-11 22:01:37 -0600

    end7 commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express"
-import mongoose from 'mongoose'
+import connectDB from "./config/db.js";
+
 
 const app = express();
-
+//Connect Database
+connectDB();
 app.get('/',(req,res) => res.send('API Running'))
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`server started on ${PORT}`))
+app.listen(PORT,()=>console.log(`Server started on ${PORT}`))
