123f551daf751b13ced311124f07b1763fa9bf8f
Nathan-Andersen <nathan.andersen91@gmail.com>
2020-11-11 22:08:57 -0600

    end7test commit

--- server.js
+++ server.js
@@ -10,3 +10,5 @@ app.get('/',(req,res) => res.send('API Running'))
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`Server started on ${PORT}`))
+
+console.log("hello world")
